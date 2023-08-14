fa618b67a7607f33f31e2b2764277aa20f5162f4
kaarvin sk <140234077+kaarvin@users.noreply.github.com>
2023-08-14 22:00:55 +0530

    Create sample program 1.py

---
+++ sample program 1.py
@@ -0,0 +1,5 @@
+print("magesh" .center(100)) --it output wil be in the terminal of centre in the region
+
+#a="   2345      .    .   "
+print(a.strip()) -- strip will be remove the white space ##remove white space at extreme (both right & left)
+
