918e8875c8209f71738bd09938b57699125a1481
kaarvin sk <140234077+kaarvin@users.noreply.github.com>
2023-08-16 18:30:31 +0530

    sample program 1.py

--- sample program 1.py
+++ sample program 1.py
@@ -1,5 +1,25 @@
 print("magesh" .center(100)) --it output wil be in the terminal of centre in the region
 
 #a="   2345      .    .   "
-print(a.strip()) -- strip will be remove the white space ##remove white space at extreme (both right & left)
+print(a.strip()) -- strip will be remove the white space ##remove white space at extreme (both right & left
 
+###FOR LOOP: -->for loop is run based only on sequence lenght;
+SAMPLE PROGRAM:
+a=[2,3,4,5,6,]
+for i in range (len(a)):
+  print(a[i])
+OUTPUT:
+2
+3
+4
+5
+6------>IT CAN BE PRINT THE VALUE IN THE LIST.
+
+###WHILE LOOP: while loop is run based on increment or decrement.
+SAMPLE PROGRAM:
+a=1 ---> starting of the loop will be what we wnat we give the number
+while (a<100):---> the condiition is based on the only where we stop
+  print(a)
+  a=a+2
+output:
+it wil print the output od 3 multiples.
